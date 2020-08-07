79e74de11d341ead63c810c4405eae2a5e7dcb42
Tsora-Pop <35981510+Tsora-Pop@users.noreply.github.com>
2020-08-07 03:29:23 -0500

    Largest Element
    This takes a predefined list and find the largest number

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn main() {
+    let number_list = vec![14,64,37,82,39];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![25,54,27,91,10];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
